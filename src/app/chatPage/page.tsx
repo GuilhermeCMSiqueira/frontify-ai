a5cc1e54935cd2e2fc9a4c2b118cef116e092eb2
Guilherme Siqueira <gcms2@cin.ufpe.br>
2024-12-20 18:24:19 -0300

    First Commit Chat Page

---
+++ src/app/chatPage/page.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { iconChat, onlineStatus, robotLogo } from "@/assets";
+import Navbar from "@/components/Navbar";
+import Image from "next/image";
+
+export default function chatPage() {
+  return (
+    <>
+      <Navbar></Navbar>
+      <div className="flex flex-row h-[calc(100vh-9em)] bg-[#12FFFF]">
+        <div className="w-full h-20 bg-[#4361EE] flex items-center justify-around">
+          <div className="flex gap-4">
+            <Image src={robotLogo} alt="Imagem de um robô" className="h-16 w-fit flex"></Image>
+            <div>
+              <h1 className="text-3xl text-white font-bold font-montserrat pt-1">Frontify.AI</h1>
+              <Image src={onlineStatus} alt={"Status online"} ></Image>
+            </div>
+            
+          </div>
+          <p></p>
+        </div>
+      </div>
+    </>
+  );
+}
--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,9 +1,13 @@
 import FrontifyLogo from './frontify-logo.png';	
 import RobotHome from './RobotHome.png';
 import iconChat from './iconChat.png';
+import robotLogo from './minimalistRobot.png';
+import onlineStatus from './onlineStatus.png';
 
 export{
     FrontifyLogo,
     RobotHome,
     iconChat,
+    robotLogo,
+    onlineStatus,
 }
