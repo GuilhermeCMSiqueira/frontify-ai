d91d3d62373e78319698a4b63cf68718118c098f
Guilherme Siqueira <gcms2@cin.ufpe.br>
2024-12-29 22:18:40 -0300

    Criação do Componente de Chat e da página de chat

--- src/app/chatPage/page.tsx
+++ src/app/chatPage/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { iconChat, onlineStatus, robotLogo } from "@/assets";
+import { Chat } from "@/components/Chat";
 import Navbar from "@/components/Navbar";
 import Image from "next/image";
 
@@ -8,18 +9,18 @@ export default function chatPage() {
   return (
     <>
       <Navbar></Navbar>
-      <div className="flex flex-row h-[calc(100vh-9em)] bg-[#12FFFF]">
+      <div className="flex flex-col gap-6 h-[calc(100vh-9em)] bg-[#12FFFF]">
         <div className="w-full h-20 bg-[#4361EE] flex items-center justify-around">
           <div className="flex gap-4">
             <Image src={robotLogo} alt="Imagem de um robô" className="h-16 w-fit flex"></Image>
             <div>
               <h1 className="text-3xl text-white font-bold font-montserrat pt-1">Frontify.AI</h1>
               <Image src={onlineStatus} alt={"Status online"} ></Image>
-            </div>
-            
+            </div>   
           </div>
           <p></p>
         </div>
+        <Chat></Chat>
       </div>
     </>
   );
