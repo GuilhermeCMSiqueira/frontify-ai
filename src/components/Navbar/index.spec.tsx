0b72f285cac76c1c44f2a61055d20ebac6b90bc1
Guilherme Siqueira <gcms2@cin.ufpe.br>
2025-02-26 20:00:57 -0300

    Navbar tests implemented

--- src/components/Navbar/index.spec.tsx
+++ src/components/Navbar/index.spec.tsx
@@ -1,8 +1,12 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import Navbar from "./index";
 import "@testing-library/jest-dom";
 import { describe, it, expect } from "@jest/globals";
+import { useRouter } from "next/router";
+import mockRouter from "next-router-mock";
+
+jest.mock("next/router", () => require("next-router-mock"));
 
 describe("Navbar", () => {
   it("should render the navbar", () => {
@@ -11,5 +15,14 @@ describe("Navbar", () => {
     expect(screen.getByText("Home")).toBeInTheDocument();
     expect(screen.getByText("Frontify AI")).toBeInTheDocument();
     expect(screen.getByText("FAQ")).toBeInTheDocument();
+  });
+
+  it("should redirect the user to the FAQ page", async () => {
+    render(<Navbar />);
+    const faq = screen.getByText("FAQ");
+    expect(faq).toBeInTheDocument();
+    fireEvent.click(faq);
+    await mockRouter.push("/faqPage");
+    expect(mockRouter.asPath).toBe("/faqPage");
   });
 });
